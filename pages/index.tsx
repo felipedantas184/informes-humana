b642faf7392dfcb2f55e4b14d35ba9e5614f5da2
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2025-04-04 13:47:25 -0300

    up year

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,17 @@ export default function HomePage() {
   return (
     <>
       <Head>
-        <title>ADUFPI | Informe de Pagamentos HUMANA 2023</title>
-        <meta name="description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2023." />
+        <title>ADUFPI | Informe de Pagamentos HUMANA 2024</title>
+        <meta name="description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2024." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:title" content="ADUFPI | Informe de Pagamentos HUMANA 2023"/>
-        <meta property="og:description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2023."/>
+        <meta property="og:title" content="ADUFPI | Informe de Pagamentos HUMANA 2024"/>
+        <meta property="og:description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2024."/>
         <meta property="og:image" content="/apple-touch-icon.png"/>
-        <meta property="og:site_name" content="ADUFPI | Informe de Pagamentos HUMANA 2023"/>
+        <meta property="og:site_name" content="ADUFPI | Informe de Pagamentos HUMANA 2024"/>
 
-        <meta property="twitter:title" content="ADUFPI | Informe de Pagamentos HUMANA 2023"/>
-        <meta property="twitter:description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2023."/>
+        <meta property="twitter:title" content="ADUFPI | Informe de Pagamentos HUMANA 2024"/>
+        <meta property="twitter:description" content="Acesse o seu informe de pagamentos referentes à HUMANA no ano de 2024."/>
         <meta property="twitter:image" content="/apple-touch-icon.png"/>
       </Head>
       <Layout>
